8e3139e679b0a49e6dfcecdeb2be2dbd7a02345f
Wesley Schneider <wesleyschneideraires@gmail.com>
2023-04-20 17:14:42 -0300

    🎉 - Cria projeto com Sprint Boot e adiciona Debezium

---
+++ src/main/java/br/com/wesleyschneider/springbootdebezium/SpringBootDebeziumApplication.java
@@ -0,0 +1,13 @@
+package br.com.wesleyschneider.springbootdebezium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDebeziumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDebeziumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wesleyschneider/springbootdebezium/SpringBootDebeziumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleyschneider.springbootdebezium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDebeziumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
